0846e3d6aa31cebadc18c6e6c5f566c5df4bfaaa
Grace Hseu <25470155+gracehseu@users.noreply.github.com>
2021-12-27 01:28:34 -0600

    added socket io, original player does not hsow up thogh

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+var players = {} // keeping track of the players
+
+// const server = require('express')();
+// const http = require('http').createServer(server);
+// const io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -7,6 +15,36 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(8080, () => {
+server.listen(8080, () => {
     console.log('Server listening on http://localhost:8080');
 });
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    // create a new player and add it to our players object
+    players[socket.id] = {
+        // rotation: 0,
+        'x': 50,
+        'y': 50,
+        'playerId': socket.id,
+        // team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
+    };
+    // send the players object to the new player
+    socket.emit('currentPlayers', players);
+    // update all other players of the new player
+    socket.broadcast.emit('newPlayer', players[socket.id]);
+    // when a player disconnects, remove them from our players object
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+        // remove this player from our players object
+        delete players[socket.id];
+        // emit a message to all players to remove this player
+        io.emit('playerDisconnected', socket.id);
+    });
+    socket.on('playerMovement', function (movementData) {
+        players[socket.id].x = movementData.x;
+        players[socket.id].y = movementData.y;
+        socket.broadcast.emit('playerMoved', players[socket.id]);
+    });
+});
+
--- public/game.js
+++ public/game.js
@@ -1,18 +1,110 @@
-const config = {
+var config = {
+    type: Phaser.AUTO,
     width: 800,
     height: 600,
-    type: Phaser.AUTO,
-    audio: {
-        disableWebAudio: true
-    },
     physics: {
         default: 'arcade',
         arcade: {
-            fps: 60,
-            gravity: { y: 0 },
+            debug: true
         }
     },
-    backgroundColor: 'blue'
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    },
+    backgroundColor: '#ADD8E6'
 };
 
-const game = new Phaser.Game(config);
+var game = new Phaser.Game(config);
+var player;
+var cursors;
+var newPlayer;
+
+function addPlayer(self, config ){
+    self.player = self.physics.add.sprite(config['x'], config['y'], 'p1')
+    console.log('added player')
+}
+
+// function addOtherPlayer(self, config) {
+//     self.physics.add.sprite(config.x, config.y, 'p2')
+//     self.otherPlayers.add(otherPlayer);
+//     console.log('added another player')
+// }
+
+function preload() {
+    // this.load.image('sky', 'bg.png');
+    this.load.image('p1', 'p1.png');
+    this.load.image('p2', 'p2.png');
+}
+
+function create() {
+    cursors = this.input.keyboard.createCursorKeys();
+    player = this.physics.add.sprite(50, 50, 'p1')
+    var self = this;
+    this.socket = io();
+    this.otherPlayers = this.physics.add.group();
+    
+    this.socket.on('currentPlayers', function (players) {
+        Object.keys(players).forEach(function (id) {
+            console.log(player)
+            if (players[id].playerId === self.socket.id) {
+                // // addPlayer(self, players[id]);
+                // player = this.physics.add.sprite(players[id]['x'], players[id]['y'], 'p1')
+                console.log('adding one single player')
+
+            } else {
+                var newPlayer = self.physics.add.sprite(config.x, config.y, 'p2');
+                newPlayer.playerId = players[id].playerId;
+                self.otherPlayers.add(newPlayer);
+            }
+            // console.log('finished for each loop')
+        });
+    });
+    this.socket.on('newPlayer', function (playerInfo) {
+        addOtherPlayer(self, playerInfo);
+    });
+    this.socket.on('playerDisconnected', function (playerId) {
+        self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+            if (playerId === otherPlayer.playerId) {
+                otherPlayer.destroy();
+            }
+        });
+    });
+
+    this.socket.on('playerMoved', function (playerInfo) {
+        console.log('player moved')
+        console.log(playerInfo)
+        console.log(self.otherPlayers)
+        self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+            if (playerInfo.playerId === otherPlayer.playerId) {
+                otherPlayer.setPosition(playerInfo.x, playerInfo.y);
+            }
+            
+        });
+    });
+    console.log('finished create')
+}
+
+function update() {
+    // console.log(player);
+    this.socket.emit('playerMovement', { 'x': player.x, 'y': player.y});
+
+
+
+    player.setVelocity(0);
+
+    if (cursors.left.isDown) {
+        player.setVelocityX(-300);
+    }
+    else if (cursors.right.isDown) {
+        player.setVelocityX(300);
+    }
+
+    if (cursors.up.isDown) {
+        player.setVelocityY(-300);
+    }
+    else if (cursors.down.isDown) {
+        player.setVelocityY(300);
+    }
+}
